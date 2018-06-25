4276fb93b788bf8d8d0d4e2e1eacbdc664a89ed8
Revanth <RevanthMS>
2018-06-25 14:12:06 +0530

    Added more comments

--- Perimeter_area_finder.py
+++ Perimeter_area_finder.py
@@ -5,16 +5,17 @@ from scipy.spatial import ConvexHull #for plotting convex polygon
 from shapely.geometry import Polygon #for finding area and perimeter
 import math
 
-#This function is to sort the random corners of the polygon in anticlock-wise direction
+#This function is to sort the random corners of the polygon in anticlock-wise direction by calculating the centroid and measuring the angles between the centroid and the centres
+
 def algo(point):
     return (math.atan2(point[0] - mean_x, point[1] - mean_y) + 2 * math.pi) % (2*math.pi)
 
-img=cv2.imread('/home/revanth/bytemInternship/images_for_programs/hexi.png')
-gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img=cv2.imread('/home/revanth/bytemInternship/images_for_programs/hexi.png')#give the path to the image you want to find the perimeter and area
+gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray scale image
 gray=np.float32(gray)
 
 #corner detection
-corners=cv2.goodFeaturesToTrack(gray,6,0.1,30)
+corners=cv2.goodFeaturesToTrack(gray,6,0.1,30) #get the number of edges that the polygon has from the user to give it in the second parameter
 corners=np.int0(corners)
 xvalues=[]
 yvalues=[]
@@ -35,17 +36,16 @@ mean_y=float(sum((yvalues)))/len(xvalues)
 
 
 point=[]
-#
+
 for i in range(len(xvalues)):
     point.append((xvalues[i],yvalues[i]))
 
 #plt.show()  
-point.sort(key=algo)
+point.sort(key=algo) #sort corners in anticlockwise direction
 sides=len(point)
-#print('The no of edges the given polygon has '+str(sides)+'!')
-#print('The coordinates of the polygon are',point)
-perimeter= Polygon(point).length
-Area= Polygon(point).area
+
+perimeter= Polygon(point).length  # this finds the perimeter by measuring the length between succesive points in the list point
+Area= Polygon(point).area # this finds the area through shoelace algorithm
 
 print ('perimeter of the polygon is',perimeter)
 print ('Area of the polygon is',Area)
@@ -55,6 +55,8 @@ hull = ConvexHull(points)
  
 #this is to plot a convex polygon for a given set of points
 #plt.plot(points[:,0], points[:,1], 'o')
+
+
 plt.plot(xvalues,yvalues,'ro')
 for simplex in hull.simplices:
     plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
